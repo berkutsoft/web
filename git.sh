e9bdb744f99f24658033e615330268468eed302b
berkutsoft <berkut1011@gmail.com>
2016-03-11 16:33:27 +0000

    web-server. задание2 v3

---
+++ git.sh
@@ -0,0 +1,4 @@
+git add * -f
+git config --global user.name "berkutsoft"
+git config --global user.email "berkut1011@gmail.com"
+#git commit -a -m "
--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo rm /etc/nginx/sites-enabled/default
-sudo ﻿ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
 sudo ln -s /home/box/web/etc/hello.py   /etc/gunicorn.d/hello.py
 sudo /etc/init.d/gunicorn restart
