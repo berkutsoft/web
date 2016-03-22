c398349f3c84dd29da4f9cdc8943ebf13f3c57a6
BerkutSoft <berkut1011@gmail.com>
2016-03-22 14:46:21 +0300

    web-server. App Ask

--- init.sh
+++ init.sh
@@ -1,6 +1,9 @@
 sudo rm /etc/nginx/sites-enabled/default
-sudo ln -s /home/box/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
+sudo ln -s ~/web/etc/nginx.conf  /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-sudo ln -s /home/box/web/etc/hello.py   /etc/gunicorn.d/hello.py
+#sudo ln -s ~/web/etc/hello.py   /etc/gunicorn.d/hello.py
+sudo ln -s ~/web/etc/gunicorn.config   /etc/gunicorn.d/ask
+#sudo ln -s ~/web/etc/django.py   /etc/gunicorn.d/django.py
 sudo /etc/init.d/gunicorn restart
-gunicorn -b 0.0.0.0:8080 -D hello:app
+#gunicorn -b 0.0.0.0:8080 -D hello:app
+#gunicorn -b 0.0.0.0:8080 -D django:app
