4244eb4a9899f0be4fecf87060795cd1399046c5
BerkutSoft <berkut1011@gmail.com>
2016-03-22 16:33:31 +0300

    web-server. App Ask. v2

--- init.sh
+++ init.sh
@@ -4,6 +4,8 @@ sudo /etc/init.d/nginx restart
 #sudo ln -s ~/web/etc/hello.py   /etc/gunicorn.d/hello.py
 sudo ln -s ~/web/etc/gunicorn.config   /etc/gunicorn.d/ask
 #sudo ln -s ~/web/etc/django.py   /etc/gunicorn.d/django.py
-sudo /etc/init.d/gunicorn restart
+#sudo /etc/init.d/gunicorn restart
 #gunicorn -b 0.0.0.0:8080 -D hello:app
 #gunicorn -b 0.0.0.0:8080 -D django:app
+cd ~/web/ask
+gunicorn -D -b 0.0.0.0:8000 ask.wsgi
