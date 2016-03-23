138c39ce04be4a8799f52eead480d980ccf616b8
berkutsoft <berkut1011@gmail.com>
2016-03-23 08:19:33 +0000

    web-server. App Ask. v4

--- ask/ask/settings.py
+++ ask/ask/settings.py
@@ -35,9 +35,9 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    #'django.contrib.admin',
+    'django.contrib.admin',
     #'django.contrib.auth',
-    #'django.contrib.contenttypes',
+    'django.contrib.contenttypes',
     #'django.contrib.sessions',
     #'django.contrib.messages',
     #'django.contrib.staticfiles',
