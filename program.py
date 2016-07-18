e367863c3e2468fa3013e3750ce73cbf8cce9128
berkutsoft <berkut1011@gmail.com>
2016-07-18 16:42:14 +0300

    mysqlalhemy v2

--- program.py
+++ program.py
@@ -3,6 +3,7 @@ from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, Unicode, BigInteger
 from sqlalchemy import exc
+import random
 
 Base = declarative_base()
 
@@ -21,16 +22,33 @@ class Order(Base):
     def __repr__(self):
         return "Name: '%s'" % (self.name)
 
+def mark_random_orders_accepted(orders):
+    count = orders.query(Order).count()
+    rnd = random.randrange(0,count)
+    print rnd
+    for i, order in enumerate(orders.query(Order).yield_per(100)):
+        if rnd==0:break
 
-db_engine = create_engine('mysql+mysqldb://root:pass@localhost/DB', echo=False)
+        order.state = 1
+        if not i % 1000:
+            print i
+            orders.commit()
+            #orders.close()
+        #print "id", i, "name", order.name
+        rnd-=1
+    print rnd
+    orders.commit()
+
+db_engine = create_engine('mysql+mysqldb://root:1011@localhost/DB', echo=False)
 
 Base.metadata.create_all(db_engine)
 Session = sessionmaker(bind=db_engine)
 session = Session()
 
 
-findHold = session.query(Order).filter(Order.state == 0).yield_per(100)
-
+#findHold = session.query(Order).filter(Order.state == 0).yield_per(100)
+findHold = session.query(Order).yield_per(100)
+mark_random_orders_accepted(session)
 #for result in findHold:
 #    print result
 
