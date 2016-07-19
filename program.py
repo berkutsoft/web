37b478f86ce8ce079fbfb36fd05b5a6becc09aec
berkutsoft <berkut1011@gmail.com>
2016-07-19 16:14:55 +0300

    Update program.py

--- program.py
+++ program.py
@@ -1,11 +1,11 @@
+#!/usr/bin/python
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import Column, Integer, Unicode, BigInteger
-from sqlalchemy import exc
-import random
 
 Base = declarative_base()
+
 
 class Order(Base):
 
@@ -13,23 +13,15 @@ class Order(Base):
 
     id = Column(BigInteger, nullable=False, primary_key=True)
     name = Column(Unicode(255), nullable=False)
-    state = Column(Integer, nullable=False, index=True)   # accepted = 1, hold = 0
+    # accepted = 1, hold = 0
+    state = Column(Integer, nullable=False, index=True)
 
     def __init__(self, name, state):
-        self.name  = name
+        self.name = name
         self.state = state
 
     def __repr__(self):
         return "Name: '%s'" % (self.name)
-
-
-def mark_random_orders_accepted(orders):
-    count = 0
-    for order in orders:
-        order.state = 1
-        count+=1
-    session.commit()
-    return count
 
 
 db_engine = create_engine('mysql+mysqldb://root:pass@localhost/DB', echo=False)
@@ -37,11 +29,15 @@ Base.metadata.create_all(db_engine)
 Session = sessionmaker(bind=db_engine)
 session = Session()
 
-count = session.query(Order).count()
-rnd = random.randrange(0,count)
 
-c = 0
-while c < rnd:
-    q = session.query(Order).filter(Order.state==0, Order.id>c, Order.id<=rnd).limit(100)
-    c += mark_random_orders_accepted(q)
-    
+def mark_random_orders_accepted(num):
+    c = 0
+    while c < num:
+        q = session.query(Order).filter(Order.state == 0).yield_per(100)
+        for order in q:
+            order.state = 1
+            c += 1
+    session.commit()
+
+if __name__ == "__main__":
+    mark_random_orders_accepted(20000)
